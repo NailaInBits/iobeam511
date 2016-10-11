96f007e7b6afea44f5dce6a67c92d49a9ecbc9ce
Nishat Anjum <nishatnanjum@gmail.com>
2016-10-11 13:28:53 -0400

    Updated API Request

--- apiRequest.js
+++ apiRequest.js
@@ -1,7 +1,7 @@
 var request = require ('superagent');
 
 request
-.get('https://511ny.org/api/getmainlinelinkstatuses?key={4eccda9d4fcb43e5acc8437f3a11ddfb}&format={json}')
+.get('https://511ny.org/api/getmainlinelinkstatuses?key={}&format={json}')
 .end(function(err, res)
 {
 	var statArray = JSON.parse(res.text);
